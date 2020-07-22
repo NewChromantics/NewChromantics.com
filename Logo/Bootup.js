04415555a8dc04e56a9065df2b52c1e09c4e67d4
Graham Reeves <graham@grahamreeves.com>
2020-07-22 12:19:29 +0100

    Adding physics, ditching svg.json

--- Logo/Bootup.js
+++ Logo/Bootup.js
@@ -14,7 +14,7 @@ SetGlobal.call(this);
 
 
 const LogoParticleShader = RegisterShaderAssetFilename('Logo/LogoParticle.frag.glsl','Logo/LogoParticle.vert.glsl');
-const LogoSdfShader = RegisterShaderAssetFilename('Logo/LogoSdf.frag.glsl','Logo/Quad.vert.glsl');
+const PositionsToSdfShader = RegisterShaderAssetFilename('Logo/LogoSdf.frag.glsl','Logo/Quad.vert.glsl');
 const BlitTextureShader = RegisterShaderAssetFilename('Logo/Blit.frag.glsl','Logo/Quad.vert.glsl');
 
 //	kick off loading
@@ -34,7 +34,7 @@ function OnParamsChanged()
 }
 Params.LocalScale = 10.0;
 Params.WorldScale = 1;
-Params.ParticleCount = 1000;
+Params.ParticleCount = 300;
 Params.DebugParticles = false;
 Params.DebugSdf = false;
 Params.BackgroundColour = [0,0,0];
@@ -58,17 +58,75 @@ ParamsWindow.AddParam('SampleDelta',0,1);
 ParamsWindow.AddParam('SampleWeightSigma',0,5,Math.floor);
 
 
-const Rect = [400,800,100,100];
+const Rect = [500,500,100,100];
 const RenderTimelineWindow = new Pop.Gui.RenderTimelineWindow("Render Stats",Rect);
 
-const LogoSdf = new Pop.Image( [256,256], 'Float4' );
-const SdfWindow = new Pop.Gui.Window('Sdf',[0,300,500,500]);
+const SdfWindow = new Pop.Gui.Window('Sdf',[0,300,300,300]);
 const SdfPreview = new Pop.Gui.ImageMap(SdfWindow,[0,0,'100%','100%']);
 
+//	texture we draw SD positions to
+const PositionsSdf = new Pop.Image( [256,256], 'Float4' );
+
+let PhysicsTextures = null;	//PhysicsTexturesManager
 
 AssetFetchFunctions['LogoParticleGeo'] = CreateLogoParticleGeo;
 AssetFetchFunctions['LogoParticlePositions'] = CreateLogoParticlePositionTexture;
 AssetFetchFunctions['Quad'] = CreateQuadGeometry;
+
+
+
+
+//	current positions, double buffered
+class PhysicsTexturesManager
+{
+	constructor(OriginalPositions)
+	{
+		this.BufferA = true;
+		//	copy originals
+		this.PositionsA = null;
+		this.PositionsB = null;
+		//	init velocitys
+		this.VelocitysA = null;
+		this.VelocitysB = null;
+	}
+	
+	GetPositions()
+	{
+		return this.BufferA ? this.PositionsA : this.PositionsB;
+	}
+
+	GetVelocitys()
+	{
+		return this.BufferA ? this.PositionsA : this.PositionsB;
+	}
+	
+	Iteration(RenderContext)
+	{
+		//	update velocities from current buffer to back buffer
+		//	update positions from current buffer to back buffer
+		//	move back buffer to front
+		this.BufferA = !this.BufferA;
+	}
+}
+
+
+function GetPositionsTexture(RenderContext)
+{
+	const WorldPositions = GetAsset('LogoParticlePositions',RenderContext);
+	return WorldPositions;
+	
+	//	init textures
+	if ( !PhysicsTextures )
+	{
+		const OriginalPositions = GetAsset('LogoParticlePositions',RenderContext);
+		PhysicsTextures = new PhysicsTexturesManager(OriginalPositions);
+		
+		//	do first iteration so things are initialised
+		PhysicsTextures.Iteration(RenderContext);
+	}
+
+	return PhysicsTextures.GetPositions();
+}
 
 
 
@@ -86,56 +144,47 @@ function GetRandomNumberArray(Count)
 }
 
 
-function CreateRandomSphereImage(Width,Height)
+//	x,y,radius
+function GetLogoPositions(SvgFilename)
 {
-	let Channels = 4;
-	let Format = 'Float4';
-	
-	const TimerStart = Pop.GetTimeNowMs();
-	
-	let Pixels = new Float32Array( Width * Height * Channels );
-	const Rands = GetRandomNumberArray(Pixels.length*Channels);
-	for ( let i=0;	i<Pixels.length;	i+=Channels )
+	const Positions = [];
+	let Bounds = null;
+	let Min = null;
+	let Max = null;
+	function OnShape(Shape)
 	{
-		let xyz = Rands.slice( i*Channels, (i*Channels)+Channels );
-		let w = xyz[3];
-		xyz = Math.Subtract3( xyz, [0.5,0.5,0.5] );
-		xyz = Math.Normalise3( xyz );
-		xyz = Math.Add3( xyz, [1,1,1] );
-		xyz = Math.Multiply3( xyz, [0.5,0.5,0.5] );
-		
-		Pixels[i+0] = xyz[0];
-		Pixels[i+1] = xyz[1];
-		Pixels[i+2] = xyz[2];
-		Pixels[i+3] = w;
+		if ( !Shape.Circle )
+		{
+			Pop.Warning(`Expected circle shape; ${JSON.stringify(Shape)} skipping`);
+			return;
+		}
+		Positions.push( [Shape.Circle.x,Shape.Circle.y,Shape.Circle.Radius] );
 	}
 	
-	Pop.Debug("CreateRandomSphereImage() took", Pop.GetTimeNowMs() - TimerStart);
+	function FixPosition(xy,SvgBounds)
+	{
+		Bounds = SvgBounds;
+		if ( Min === null )	Min = xy.slice();
+		if ( Max === null )	Max = xy.slice();
+		Min[0] = Math.min(xy[0],Min[0]);
+		Min[1] = Math.min(xy[1],Min[1]);
+		Max[0] = Math.max(xy[0],Max[0]);
+		Max[1] = Math.max(xy[1],Max[1]);
+		return xy;
+	}
 	
-	let Texture = new Pop.Image();
-	Texture.WritePixels( Width, Height, Pixels, Format );
-	return Texture;
+	const SvgContents = Pop.LoadFileAsString( SvgFilename );
+	Pop.Svg.ParseShapes(SvgContents,OnShape,FixPosition);
+	
+	return Positions;
 }
-
 
 function LoadLogoParticles(RenderTarget)
 {
-	const Positions = [];
-	function PushVertex(x,y,z,Radius)
-	{
-		Positions.push( x );
-		Positions.push( y );
-		Positions.push( z );
-		Positions.push( Radius );
-	}
-	
-	const GeoFilename = 'Logo/Logo.svg.json';
-	const GeoContents = Pop.LoadFileAsString( GeoFilename );
-	Pop.Svg.Parse( GeoContents, PushVertex );
-	
+	const Positions = GetLogoPositions('Logo/Logo.svg');
 	const Attributes = {};
 	const PositionAttribute = {};
-	PositionAttribute.Size = 4;
+	PositionAttribute.Size = 3;
 	PositionAttribute.Data = new Float32Array( Positions );
 	Attributes['PositionRadius'] = PositionAttribute;
 	
@@ -144,6 +193,34 @@ function LoadLogoParticles(RenderTarget)
 	return TriangleBuffer;
 }
 
+
+function CreateLogoParticlePositionTexture(RenderTarget)
+{
+	const Positions = GetLogoPositions('Logo/Logo.svg');
+	
+	//Params.WorldScale = Bounds[2] / Bounds[3];
+	ParamsWindow.OnParamChanged('WorldScale');
+	Params.ParticleCount = Positions.length;
+	
+	//	turn points into image
+	const Width = 1024;
+	const Height = Math.GetNextPowerOf2( Positions.length / Width );
+	const Channels = 3;
+	const Pixels = new Float32Array( Width * Height * Channels );
+	function PushPixel(xyr,Index)
+	{
+		const p = Index * Channels;
+		Pixels[p+0] = xyr[0];
+		Pixels[p+1] = xyr[1];
+		Pixels[p+2] = xyr[2];
+	}
+	Positions.forEach( PushPixel );
+	
+	const Format = 'Float' + Channels;
+	const Image = new Pop.Image('LogoPositions');
+	Image.WritePixels( Width, Height, Pixels, Format );
+	return Image;
+}
 
 function CreateLogoParticleGeo(RenderTarget)
 {
@@ -202,70 +279,18 @@ function CreateQuadGeometry(RenderTarget)
 }
 
 
-function CreateLogoParticlePositionTexture(RenderTarget)
-{
-	const Positions = [];
-	let Bounds = null;
-	let Min = null;
-	let Max = null;
-	function OnShape(Shape)
-	{
-		if ( !Shape.Circle )
-		{
-			Pop.Warning(`Expected circle shape; ${JSON.stringify(Shape)} skipping`);
-			return;
-		}
-		Positions.push( [Shape.Circle.x,Shape.Circle.y,Shape.Circle.Radius] );
-	}
-
-	function FixPosition(xy,SvgBounds)
-	{
-		Bounds = SvgBounds;
-		if ( Min === null )	Min = xy.slice();
-		if ( Max === null )	Max = xy.slice();
-		Min[0] = Math.min(xy[0],Min[0]);
-		Min[1] = Math.min(xy[1],Min[1]);
-		Max[0] = Math.max(xy[0],Max[0]);
-		Max[1] = Math.max(xy[1],Max[1]);
-		return xy;
-	}
-	
-	const SvgFilename = 'Logo/Logo.svg';
-	const SvgContents = Pop.LoadFileAsString( SvgFilename );
-	Pop.Svg.ParseShapes(SvgContents,OnShape,FixPosition);
-
-	//Params.WorldScale = Bounds[2] / Bounds[3];
-	ParamsWindow.OnParamChanged('WorldScale');
-	Params.ParticleCount = Positions.length;
-
-	//	turn points into image
-	const Width = 1024;
-	const Height = Math.GetNextPowerOf2( Positions.length / Width );
-	const Channels = 3;
-	const Pixels = new Float32Array( Width * Height * Channels );
-	function PushPixel(xyr,Index)
-	{
-		const p = Index * Channels;
-		Pixels[p+0] = xyr[0];
-		Pixels[p+1] = xyr[1];
-		Pixels[p+2] = xyr[2];
-	}
-	Positions.forEach( PushPixel );
-	
-	const Format = 'Float' + Channels;
-	const Image = new Pop.Image('LogoPositions');
-	Image.WritePixels( Width, Height, Pixels, Format );
-	return Image;
-}
 
 
 
 function RenderSdf(RenderTarget,AspectRect)
 {
+	//	gr: asset fetching should be context
+	//const RenderContext = RenderTarget.GetRenderContext();
+	const RenderContext = RenderTarget;
 	RenderTarget.ClearColour( 0,0,0 );
 	
-	const Shader = GetAsset(LogoParticleShader,RenderTarget);
-	const WorldPositions = GetAsset('LogoParticlePositions',RenderTarget);
+	const Shader = GetAsset(LogoParticleShader,RenderContext);
+	const WorldPositions = GetPositionsTexture(RenderContext);
 	const Geo = GetAsset('LogoParticleGeo',RenderTarget);
 	const LocalPositions = [	-1,-1,0,	1,-1,0,	0,1,0	];
 	const RenderTargetRect = RenderTarget.GetRenderTargetRect();
@@ -304,22 +329,22 @@ function Render(RenderTarget)
 	}
 	
 	//	update sdf
-	const ReadBack = true;
-	RenderTarget.RenderToRenderTarget( LogoSdf, CallRenderSdf, ReadBack );
-	if ( !SdfWindow.IsMinimised() )
-		SdfPreview.SetImage(LogoSdf);
+	const ReadBack = !SdfWindow.IsMinimised();
+	RenderTarget.RenderToRenderTarget( PositionsSdf, CallRenderSdf, ReadBack );
+	if ( ReadBack )
+		SdfPreview.SetImage(PositionsSdf);
 	
 	//	turn sdf into image
 	RenderTarget.ClearColour( 0,1,0 );
 
-	const Shader = GetAsset( Params.DebugSdf ? BlitTextureShader : LogoSdfShader, RenderTarget);
+	const Shader = GetAsset( Params.DebugSdf ? BlitTextureShader : PositionsToSdfShader, RenderTarget);
 	const Geo = GetAsset('Quad',RenderTarget);
 	const RenderTargetRect = RenderTarget.GetRenderTargetRect();
 	const AspectRatio = RenderTargetRect[3] / RenderTargetRect[2];
 	
 	const SetUniforms = function(Shader)
 	{
-		Shader.SetUniform('Texture',LogoSdf);
+		Shader.SetUniform('Texture',PositionsSdf);
 		Shader.SetUniform('ProjectionAspectRatio',AspectRatio);
 		
 		function SetUniform(Key)
